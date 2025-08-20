f177b6deeb7b191a2fa396658dc7d2757d405edc
rla200 <rlarka9958@gmail.com>
2025-08-20 12:01:18 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -5,3 +5,10 @@ $(function () {
 	});
 
 });
+$(function () {
+	$("[id^=btn]").click(function () {
+		let index = $(this).attr("id").replace("btn", "");
+		$(".image" + index).fadeIn().siblings().fadeOut();
+	});
+
+});
